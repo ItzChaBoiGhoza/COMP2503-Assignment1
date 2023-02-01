a501dd421e5d0f46e61116178cc5e0eca88a3fa0
Ghoza <cguy925@gmail.com>
2023-01-31 22:44:17 -0700

    Initial commit

--- A1Starter Code and Sample Input Output/Avenger.java
+++ A1Starter Code and Sample Input Output/Avenger.java
@@ -1,12 +1,27 @@
-
 public class Avenger implements Comparable <Avenger>{
 
-	// TODO: Implement the Avenger Class
+	private String heroName;
+	private String heroAlias;
+	private int frequency;
+	
+	public Avenger( ) {
+		heroName = "";
+		heroAlias = "";
+		frequency = 0;
+	}
 	
 	@Override
 	public int compareTo(Avenger o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.heroAlias.compareTo(o.heroAlias);
 	}
-
+	
+	public int getFrequency() {return frequency;}
+	public void setName(String name) {heroName = name;}
+	public void setAlias(String alias) {heroAlias = alias;}
+	public String getName() {return heroName;}
+	public String getAlias() {return heroAlias;}
+	
+	public String toString() {
+		return heroAlias + " aka " + heroName + " mentioned " + frequency + " time(s)";
+	}
 }
