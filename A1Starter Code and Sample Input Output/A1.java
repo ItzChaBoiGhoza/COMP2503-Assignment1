dc58c0746e70822be2eaef3306aa35ace715acda
Ghoza <cguy925@gmail.com>
2023-01-31 22:36:13 -0700

    Added file scanner inside A1

--- A1Starter Code and Sample Input Output/A1.java
+++ A1Starter Code and Sample Input Output/A1.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /** 
  * COMP 2503 Winter 2023 Assignment 1 
@@ -21,12 +24,12 @@ public class A1 {
 	private int totalwordcount = 0;
 	private ArrayList<Avenger> avengersArrayList = new ArrayList<>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		A1 a1 = new A1();
 		a1.run();
 	}
 
-	public void run() {
+	public void run() throws FileNotFoundException {
 		readInput();
 		printResults();
 	}
@@ -34,8 +37,11 @@ public class A1 {
 	/**
 	 * read the input stream and keep track  
 	 * how many times avengers are mentioned by alias or last name.
+	 * @throws FileNotFoundException 
 	 */
-	private void readInput() {
+	
+
+	private void readInput() throws FileNotFoundException {
 		/*
 		In a loop, while the scanner object has not reached end of stream,
 		 	- read a word.
@@ -45,7 +51,22 @@ public class A1 {
 				- Create a new avenger object with the corresponding alias and last name.
 				- if this avenger has already been mentioned, increase the frequency count for the object already in the list.
 				- if this avenger has not been mentioned before, add the newly created avenger to the list, remember to set the frequency.
-		*/ 
+		*/
+		File file = new File("./input3.txt");
+		Scanner fileReader = new Scanner(file);
+		
+		while(fileReader.hasNext()) {
+			String word = fileReader.next().trim().toLowerCase().replaceAll("[^a-z]", "").replaceAll("[0123456789]","").replaceAll("'s","");
+			if(word.length() > 0) {
+				totalwordcount++;
+				for(int i = 0; i < avengerRoster.length; i++) {
+					if(word.equals(avengerRoster)) {
+						avengersArrayList.add(null);
+					}
+				}
+			}
+		}
+		fileReader.close();
 	}
 
 	/**
@@ -59,6 +80,7 @@ public class A1 {
 		System.out.println("All avengers in the order they appeared in the input stream:");
 		// Todo: Print the list of avengers in the order they appeared in the input
 		// Make sure you follow the formatting example in the sample output
+		
 		System.out.println();
 		
 		System.out.println("Top " + topN + " most popular avengers:");
